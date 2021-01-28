62d324da7fab03d270f9404b5d056ab8d3ce04aa
Jesusmcz <jesus.contreras.z@hotmail.cl>
2021-01-27 22:11:38 -0300

    avance eva6_pacientes

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Paciente(models.Model):
+    run = models.CharField(max_length=9, primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    edad = models.IntegerField()
+
+
+
---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name="app"
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('lista/', views.ListaPaciente.as_view(),name='lista'),
+    path('crear', views.CrearPaciente.as_view(), name='crear'),
+    path('<int:pk>/editar', views.EditarPaciente.as_view(), name='editar'),
+    path('<int:pk>/eliminar', views.EliminarPaciente.as_view(), name='eliminar'),
+]
